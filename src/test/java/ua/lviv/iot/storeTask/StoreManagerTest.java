d1fb6fde011b08659d20232e8c1e5fa85df82011
Liza Dudyk <55206822+lizooo@users.noreply.github.com>
2020-03-11 09:16:32 +0200

    Added files for laboratory work no. 4

--- src/test/java/ua/lviv/iot/storeTask/StoreManagerTest.java
+++ src/test/java/ua/lviv/iot/storeTask/StoreManagerTest.java
@@ -8,44 +8,42 @@ import java.util.List;
 
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
-
 import ua.lviv.iot.storeTask.Manager.StoreManager;
 import ua.lviv.iot.storeTask.Model.AbstractProduct;
 import ua.lviv.iot.storeTask.Model.AgeGroup;
 
 class StoreManagerTest extends BaseStoreManagerTest {
 
-	private StoreManager storeManager;
-	List<AbstractProduct> expectedFoundproducts = new LinkedList<AbstractProduct>();
+  private StoreManager storeManager;
+  List<AbstractProduct> expectedFoundproducts = new LinkedList<AbstractProduct>();
 
-	@BeforeEach
-	public void setUp() {
-		storeManager = new StoreManager();
-		storeManager.addproducts(products);
-	}
+  @BeforeEach
+  public void setUp() {
+    storeManager = new StoreManager();
+    storeManager.addproducts(products);
+  }
 
-	@Test
-	public void testFindByPrice() {
-		List<AbstractProduct> foundProducts = storeManager.findByPrice(60.00);
-		assertEquals(4, foundProducts.size());
-		assertEquals(75.25, foundProducts.get(0).getPriceInUAH());
-		assertEquals(90.00, foundProducts.get(1).getPriceInUAH());
-		assertEquals(150.75, foundProducts.get(2).getPriceInUAH());
-	}
+  @Test
+  public void testFindByPrice() {
+    List<AbstractProduct> foundProducts = storeManager.findByPrice(60.00);
+    assertEquals(4, foundProducts.size());
+    assertEquals(75.25, foundProducts.get(0).getPriceInUAH());
+    assertEquals(90.00, foundProducts.get(1).getPriceInUAH());
+    assertEquals(150.75, foundProducts.get(2).getPriceInUAH());
+  }
 
-	@Test
-	public void testFindForAgeGroup() {
-		List<AbstractProduct> foundProducts = storeManager.findForAgeGroup(AgeGroup.ONE_TWO_MONTH);
-		assertEquals(1, foundProducts.size());
-	}
+  @Test
+  public void testFindForAgeGroup() {
+    List<AbstractProduct> foundProducts = storeManager.findForAgeGroup(AgeGroup.ONE_TWO_MONTH);
+    assertEquals(1, foundProducts.size());
+  }
 
-	@Test
-	public void testCheckIfExpired() throws ParseException {
-		if (products.get(3).checkIfExpired() == false) {
-			int var = 1;
-			assertEquals(1, var);
-		}
-	}
-
+  @Test
+  public void testCheckIfExpired() throws ParseException {
+    if (products.get(3).checkIfExpired() == false) {
+      int var = 1;
+      assertEquals(1, var);
+    }
+  }
 
 }
